ea044aa8df247f02b390b91bdb76ee8f4d7588d3
zky_1031 <597768024@qq.com>
2023-02-18 14:23:56 +0800

    improve the code accroding to the review

--- server/src/main/java/com/CatApplication.java
+++ server/src/main/java/com/CatApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatApplication.class,args);
-        System.out.println("hello world");
     }
 }
